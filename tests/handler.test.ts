a910fae09cb1dc9dda78f98120d575c28521341b
Rohit <rohit.chillar31@gmail.com>
2021-12-16 00:16:55 +0530

    Solana Blockchain data

--- tests/handler.test.ts
+++ tests/handler.test.ts
@@ -5,8 +5,11 @@ test("hello", async () => {
   const event = { body: "Test Body" } as APIGatewayEvent;
   const context = {} as Context;
 
-  const response = await handler.hello(event, context);
+  const callback = (error, response) => {
+    expect(response.statusCode).toEqual(200);
+    expect(typeof response.body).toBe("string");
+  };
 
-  expect(response.statusCode).toEqual(200);
-  expect(typeof response.body).toBe("string");
+  await handler.hello(event, context,callback);
+
 });
