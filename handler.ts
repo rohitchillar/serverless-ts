78ccfb2c113ba3c0ba7a92d1fc89a4432d0b5c04
Rohit <rohit.chillar31@gmail.com>
2021-12-17 13:00:13 +0530

    added blockchain functions

--- handler.ts
+++ handler.ts
@@ -1,6 +1,8 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
-import { PublicKey, Connection } from '@solana/web3.js';
-import { AccountLayout,AccountInfo as TokenAccountInfo,u64 } from '@solana/spl-token';
+import { PublicKey, Connection,clusterApiUrl } from '@solana/web3.js';
+import { AccountLayout,MintLayout, AccountInfo as TokenAccountInfo,u64 } from '@solana/spl-token';
+import {getMetadata} from './helpers/accounts';
+import { decodeMetadata, Metadata } from './helpers/schema';
 
 type StringPublicKey = string;
 
@@ -81,8 +83,11 @@ const TokenAccountParser = (
 
 async function getInfo() {
 
-  const rpcUrl = 'https://api.devnet.solana.com';
-  const connection = new Connection(rpcUrl, 'confirmed');
+  //const rpcUrl = 'https://api.devnet.solana.com';
+  //const connection = new Connection(rpcUrl, 'confirmed');
+
+  const connection = new Connection(clusterApiUrl('devnet'));
+
   const pubkey=new PublicKey('sfgArd1pGpQZVxqoA8LKxEozeNQSxXbBTEa5CFotpeN');
   const accounts = await connection.getTokenAccountsByOwner(pubkey, {
     programId: new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"),
@@ -97,10 +102,69 @@ async function getInfo() {
     ));
   });
 
-  return arr;
+  console.log(arr[3]);
+  //decimals=1 and amount=1
+
+  // const metadata=await getMetadata(arr[3].info.mint)
+
+  // const metadataObj = await connection.getAccountInfo(
+  //   metadata,
+  // );
+  // const metadataDecoded: Metadata = decodeMetadata(
+  //   Buffer.from(metadataObj.data),
+  // );
+
+  // console.log(metadataDecoded);
+
+  //const mintInfo = await token.getMintInfo();
+
+  return 1
+
 }
 
+//   arr.filter { acct ->
+//     acct.account.data.parsed.info.tokenAmount.amount == 1.0 &&
+//         acct.account.data.parsed.info.tokenAmount.decimals == 0.0
+// }.forEach {
+//     val mintAddress = it.account.data.parsed.info.mint
+
+//     val pdaSeeds = listOf(
+//         MetaplexContstants.METADATA_NAME.toByteArray(),
+//         Base58.decode(MetaplexContstants.METADATA_ACCOUNT_PUBKEY),
+//         Base58.decode(mintAddress)
+//     )
+
+//     val pdaAddr = PublicKey.findProgramAddress(
+//         pdaSeeds,
+//         PublicKey(MetaplexContstants.METADATA_ACCOUNT_PUBKEY)
+//     )
+
+//     val accountInfo = accountsRepository.getAccountInfo(pdaAddr.address)
+//     try {
+//         val borshData = Base64.getDecoder().decode(accountInfo.data[0])
+//         val metaplexData: MetaplexMeta = borsh.deserialize(borshData, MetaplexMeta::class.java)
+
+//         // Sometimes the borsh-deserialized data has NUL chars on the end, so we need to sanitize
+//         val sanitizedUri = metaplexData.data.uri.replace("\u0000", "")
+
+//         val details = withContext(Dispatchers.IO) {
+//             nftSpecRepository.getNftDetails(sanitizedUri)
+//         }
+//         details?.let { item -> metaplexNfts.add(item) }
+//     } catch (e: Exception) {
+//         Log.e("SOL", "Attached data is not Metaplex Meta format", e)
+//     }
+// }
+// } catch (e: Exception) {
+// Log.e("SOL", "Error attempting to load nfts for address $address", e)
+// }
+
+// return metaplexNfts
+// }
+// }
+
 export const hello: APIGatewayProxyHandler = async (event, context) => {
+  //Whether wallet already exists in our database ? If yes then get the data from dynamo and return else get the data from blockchain, update in dynamo and return
   const accounts = await getInfo();
   return {
     statusCode: 200,
